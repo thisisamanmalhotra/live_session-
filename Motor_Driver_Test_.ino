88067d5f7ea5725ed41677e1f58c727b9c03540c
Aman Malhotra <33927579+thisisamanmalhotra@users.noreply.github.com>
2020-03-06 10:54:59 +0530

    Initial Motor Driver test
    V1.0

---
+++ Motor_Driver_Test_.ino
@@ -0,0 +1,24 @@
+int A = 9; 
+int B = 8;   
+int C = 7;
+int D = 6;     
+void setup()
+{
+  pinMode(A, OUTPUT);
+  pinMode(B, OUTPUT);
+  pinMode(C, OUTPUT);
+  pinMode(D, OUTPUT);
+}
+
+void loop()
+ {
+  digitalWrite(A,HIGH)
+  delay(300);
+  digitalWrite(B,HIGH)
+  delay(300);
+  digitalWrite(C,HIGH)
+  delay(300);
+  digitalWrite(D,HIGH)
+  delay(300);
+
+}
